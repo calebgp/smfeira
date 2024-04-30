9853e219daa3f149a59807dbdc89233114d4a097
Caleb Gomes Pinto <caleb.gp.dev@gmail.com>
2024-04-30 10:09:10 +0000

    feat: 2 operations added to menu

--- psi/pp/menu-sql/main.py
+++ psi/pp/menu-sql/main.py
@@ -38,7 +38,23 @@ def insert_data(con, cur):
 def list_data(con, cur):
     try:
         cur.execute("""SELECT * FROM ginasios""")
-        print("Listagem de Ginásios:")
+        print("Listagem de Ginásios Ordenados Pela Localidade:")
+        for linha in cur.fetchall():
+            print(linha)
+    except Exception as e:
+        print(f"Erro ao listar dados: {e}")
+def list_data_sorted(con, cur):
+    try:
+        cur.execute("""SELECT * FROM ginasios order by localidade""")
+        print("Listagem de Ginásios Ordenados pela localidade:")
+        for linha in cur.fetchall():
+            print(linha)
+    except Exception as e:
+        print(f"Erro ao listar dados: {e}")
+def list_code_num_atletas(con, cur):
+    try:
+        cur.execute("""SELECT codigo_ginasio, num_atletas FROM ginasios""")
+        print("Listagem Campos do Ginásio (código, número de atletas):")
         for linha in cur.fetchall():
             print(linha)
     except Exception as e:
@@ -66,8 +82,10 @@ def main():
         print("1 - Criar Tabela")
         print("2 - Inserir Dados")
         print("3 - Listar Dados")
-        print("4 - Eliminar Dados")
-        print("5 - Sair")
+        print("4 - Listar Dados Ordenados Pela Localidade")
+        print("5 - Listar Campos do Ginásio (código, número de atletas)")
+        print("6 - Eliminar Dados")
+        print("7 - Sair")
     
         try:
             opcao = int(input("Escolha uma opção: "))
@@ -78,9 +96,14 @@ def main():
                 insert_data(con, cur)
             elif opcao == 3:
                 list_data(con, cur)
+            
             elif opcao == 4:
-                delete_data(con, cur)
+                list_data_sorted(con, cur)
             elif opcao == 5:
+                list_code_num_atletas(con, cur)
+            elif opcao == 6:
+                delete_data(con, cur)
+            elif opcao == 7:
                 print("Saindo do programa...")
                 break
             else:
