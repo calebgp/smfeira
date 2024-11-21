6c0de4c068343eca19af0f0cbff88839a9d54968
Caleb Gomes Pinto <caleb.gp.dev@gmail.com>
2024-11-21 14:45:12 +0000

    feat: contatos e fundacao pages

---
+++ psi/domingos_oliveira/luis_figo_page/index.js
@@ -0,0 +1,52 @@
+// Função para carregar o conteúdo de cada página na div "content"
+function loadContent(page) {
+    fetch(page)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('content').innerHTML = data;
+            updateActiveLink(page);
+        })
+        .catch(error => console.error('Erro ao carregar a página:', error));
+}
+
+// Função para atualizar o link ativo na navegação
+function updateActiveLink(path) {
+    const navLinks = document.querySelectorAll('.topnav a');
+    navLinks.forEach(link => link.classList.remove('active'));
+
+    document.querySelector(`.topnav a[href='${path}']`).classList.add('active');
+}
+
+// Função para navegar sem recarregar a página e atualizar o URL
+function navigateTo(event, path) {
+    if (event) event.preventDefault(); // Previne o comportamento padrão de recarregar a página
+    history.pushState({}, "", path); // Atualiza o URL sem recarregar a página
+    loadPageFromPath(path); // Carrega o conteúdo correto baseado no novo URL
+}
+
+// Função para determinar o conteúdo a ser carregado baseado no caminho
+function loadPageFromPath(path) {
+    switch (path) {
+        case "/contatos/":
+            loadContent("/contatos");
+            break;
+        case "/fundacao/":
+            console.log("Fundacao");
+            loadContent("/fundacao");
+            break;
+        case "/home/":
+            loadContent("/home");
+            break;
+        default:
+            loadContent("/home");
+            break;
+    }
+}
+
+// Manipula o evento de navegação do navegador (ex: botão "Voltar")
+window.onpopstate = () => {
+    loadPageFromPath(window.location.pathname);
+};
+
+const path = window.location.pathname;
+loadPageFromPath(path);
